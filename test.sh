f5f1878f2af1dcbdbc17053a77374c82ff7831c7
Bernd Herding <bernd.herding@external.biontech.com>
2026-06-10 16:44:14 +0200

    add test script

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+GLOBAL_ACCELERATOR_URL=$1
+NUMBER_CURL_CALLS=${2:-40}
+
+(
+  trap 'exit 0' INT
+  i=0
+  while [ $i -lt $NUMBER_CURL_CALLS ] ; do
+    curl -s "$GLOBAL_ACCELERATOR_URL" \
+    | grep Instance \
+    | sed -e 's/^.* \(.*\)<.*/\1/g'
+    sleep 0.1
+    i=$((i + 1))
+  done
+) | tee test.data
+
+echo
+
+cat test.data | awk '
+  {
+    s[$0]+=1
+    n+=1
+  }
+
+  END {
+    for (i in s) {
+      printf("%4.1f%% %3d   %s\n", 100*s[i]/n, s[i], i)
+    }
+  }
+' | sort -nr -k2
