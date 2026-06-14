7ff515b669f51c588f87ddcad425c352eeb2313e
Bernd Herding <bernd.herding@external.biontech.com>
2026-06-15 00:09:47 +0200

    add demo to userdata

--- launch-template-userdata.tf
+++ launch-template-userdata.tf
@@ -5,6 +5,35 @@ data "cloudinit_config" "web" {
   base64_encode = true
 
   part {
+    filename     = "00-cloud-config.yaml"
+    content_type = "text/cloud-config"
+    content      = <<-EOC
+      #cloud-config
+      write_files:
+        - path: /var/www/html/architecture.drawio.svg
+          permissions: '0644'
+          owner: root:root
+          encoding: b64
+          content: ${base64encode(file("${path.module}/assets/architecture.drawio.svg"))}
+        - path: /var/www/html/architecture.inverted.svg
+          permissions: '0644'
+          owner: root:root
+          encoding: b64
+          content: ${base64encode(file("${path.module}/assets/architecture.inverted.svg"))}
+        - path: /var/www/html/architecture.black.svg
+          permissions: '0644'
+          owner: root:root
+          encoding: b64
+          content: ${base64encode(file("${path.module}/assets/architecture.inverted.svg"))}
+        - path: /var/www/html/demo.html
+          permissions: '0644'
+          owner: root:root
+          encoding: b64
+          content: ${base64encode(file("${path.module}/assets/demo.html"))}
+    EOC
+  }
+
+  part {
     filename     = "01-start-proxyserver.py"
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/cloudinit/01-start-proxyserver.py", {
