61d9ab45ee4fc064d1f1e15fe05e65bd4c3a3da2
Bernd Herding <bernd.herding@external.biontech.com>
2026-06-15 15:44:25 +0200

    fix s3 object name

--- s3-assets.tf
+++ s3-assets.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_public_access_block" "assets" {
 locals {
   _asset_files = {
     "architecture.drawio.svg" = "${path.module}/assets/architecture.drawio.svg"
-    "architecture.demo.svg"   = "${path.module}/assets/architecture.demo.svg"
+    "architecture.demo.svg"   = "${path.module}/assets/architecture.black.svg"
     "demo.html"               = "${path.module}/assets/demo.html"
   }
 }
